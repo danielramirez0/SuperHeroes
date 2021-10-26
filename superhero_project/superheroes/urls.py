b59801f58cb4af7b79606f627cab2c85f0ff0003
Daniel Ramirez <zoerunner@gmail.com>
2021-10-25 19:52:14 -0700

    Add create function and template to add a new hero to the database
    Add tempalte file to commit history

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -7,4 +7,5 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>', views.detail, name='detail'),
+    path('new/', views.create, name='create')
 ]
