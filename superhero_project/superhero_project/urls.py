c6198d7d0f0df30505febea3cee187bd2a7a52a6
Daniel Ramirez <zoerunner@gmail.com>
2021-10-25 18:56:26 -0700

    Add index template in view

--- superhero_project/superhero_project/urls.py
+++ superhero_project/superhero_project/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('superheroes/', include('superheroes.urls')),
-    path('', RedirectView.as_view(url='/superheroes/index/'))
+    path('', RedirectView.as_view(url='/superheroes/'))
 ]
